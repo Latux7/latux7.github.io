311ade3f8076855a663bae9e455fba81c4616fed
Hossein <hosseinrezai9@gmail.com>
2025-09-15 18:59:11 +0200

    Add Firebase configuration file: move sensitive keys from .gitignore to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAj3x8c439IEqOLe7U_yFPsywwXhZRsIog",
+  authDomain: "lauras-backstube.firebaseapp.com",
+  projectId: "lauras-backstube",
+  storageBucket: "lauras-backstube.firebasestorage.app",
+  messagingSenderId: "115340979108",
+  appId: "1:115340979108:web:70c2c46052045c5e366f31",
+  measurementId: "G-R51RPWTCFZ"
+};
